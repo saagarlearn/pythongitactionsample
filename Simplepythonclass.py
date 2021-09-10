c5355000cbdb3e747a069595dcb30eba5f852e9c
sagar <saagar.learn@gmail.com>
2021-09-10 21:47:50 +0100

    commit

---
+++ Simplepythonclass.py
@@ -0,0 +1,9 @@
+class SimpleClient:
+    def __init__(self, host):
+        self.host = host
+
+    def listModelEditions(self, model_name):
+      print("The model list name is: "+model_name)
+
+    def SecondFunc(self,second):
+        print("Second: "+second)
